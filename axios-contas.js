a03b47cc02eb92421d2738e6f3012402517373f1
Tiago Krebs <krebstiago@gmail.com>
2019-08-17 18:15:55 -0300

    backend instance para shared

--- axios-contas.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// instância de HTTP client para API backend v1
-const instance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1/'
-});
-
-export default instance;
